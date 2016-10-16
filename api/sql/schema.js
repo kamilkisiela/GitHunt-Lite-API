f2311e5f6146c9f6180389f674d2d2b2f69c7876
Kamil Kisiela <kamil.kisiela@gmail.com>
2016-10-16 17:17:43 +0200

    12. Expose Entry.repository

--- api/sql/schema.js
+++ api/sql/schema.js
@@ -15,6 +15,7 @@ export const schema = [`
     createdAt: Float! # Actually a date
     postedBy: String!
     id: Int!
+    repository: Repository!
   }
 
 `];
@@ -23,6 +24,9 @@ export const resolvers = {
   Entry: {
     // id: property('id') - GraphQL looks for properties with the same name 
     postedBy: property('posted_by'), // because "postedBy" != "posted_by" 
-    createdAt: property('created_at') // the same
+    createdAt: property('created_at'), // the same
+    repository(root, _, context) {
+      return context.Repositories.getByFullName(root.repository_name);
+    },
   }
 };
