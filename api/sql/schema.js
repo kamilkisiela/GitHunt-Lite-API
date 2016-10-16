7ec5af5cb2cef114b73cdee328c52a3919f5ccf6
Kamil Kisiela <kamil.kisiela@gmail.com>
2016-10-16 16:32:07 +0200

    9. Define Entry type with basic fields

---
+++ api/sql/schema.js
@@ -0,0 +1,28 @@
+import { property } from 'lodash';
+
+// how property function works:
+//    var objects = [
+//      { 'a': { 'b': 2 } },
+//      { 'a': { 'b': 1 } }
+//    ];
+//     
+//    _.map(objects, _.property('a.b'));
+//    // => [2, 1]
+
+export const schema = [`
+
+  type Entry {
+    createdAt: Float! # Actually a date
+    postedBy: String!
+    id: Int!
+  }
+
+`];
+
+export const resolvers = {
+  Entry: {
+    // id: property('id') - GraphQL looks for properties with the same name 
+    postedBy: property('posted_by'), // because "postedBy" != "posted_by" 
+    createdAt: property('created_at') // the same
+  }
+};
